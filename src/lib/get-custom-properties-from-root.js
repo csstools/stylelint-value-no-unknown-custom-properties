11525feacbcf441bc328cfae8a52ee9e610df2f3
Not Harrison Smith <harrison@pebblepad.co.uk>
2022-01-25 18:48:32 +0000

    fixed import cache definition causing test failures

--- src/lib/get-custom-properties-from-root.js
+++ src/lib/get-custom-properties-from-root.js
@@ -1,6 +1,8 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import process from 'process';
 import postcss from 'postcss';
+import {importCache} from "./import-cache";
 
 // return custom selectors from the css root, conditionally removing them
 export default async function getCustomPropertiesFromRoot(root) {
@@ -17,8 +19,22 @@ export default async function getCustomPropertiesFromRoot(root) {
 	const importPromises = [];
 	root.walkAtRules('import', atRule => {
 		const fileName = atRule.params.replace(/['|"]/g, '');
-		const resolvedFileName = path.resolve(sourceDir, fileName);
-		importPromises.push(getCustomPropertiesFromCSSFile(resolvedFileName));
+		let resolvedFileName;
+
+		if ((/[~]/g).test(fileName)) {
+			path.resolve(sourceDir, fileName);
+			resolvedFileName = path.normalize(fileName.replace(/[~]/g, path.join(process.cwd(), 'node_modules') + "/"));
+		} else {
+			resolvedFileName = path.resolve(sourceDir, fileName);
+		}
+
+		// if a cache doesn't exist, create it
+		if (!importCache.has(resolvedFileName)) {
+			importCache.set(resolvedFileName, getCustomPropertiesFromCSSFile(resolvedFileName));
+		}
+
+		// use the cached custom properties
+		importPromises.push(importCache.get(resolvedFileName));
 	});
 
 	(await Promise.all(importPromises)).forEach(propertiesFromImport => {
---
+++ src/lib/import-cache.js
@@ -0,0 +1,1 @@
+export const importCache = new Map();
