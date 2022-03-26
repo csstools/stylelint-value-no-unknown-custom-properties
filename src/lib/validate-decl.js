79baf1142cd55c1f94f7211a72187842d1ba28f9
Antonio Laguna <sombragriselros@gmail.com>
2022-03-26 22:32:07 +0100

    Getting rid of postcss-values-parser

--- src/lib/validate-decl.js
+++ src/lib/validate-decl.js
@@ -1,48 +1,50 @@
 import stylelint from 'stylelint';
-import { parse } from 'postcss-values-parser';
+import valueParser from 'postcss-value-parser';
 import ruleName from './rule-name';
 import messages from './messages';
 
 // validate css declarations
 export default (decl, { result, customProperties }) => {
-	const valueAST = parse(decl.value);
+	const valueAST = valueParser(decl.value);
 
 	validateValueAST(valueAST, { result, customProperties, decl });
 };
 
 // validate a value ast
 const validateValueAST = (ast, { result, customProperties, decl }) => {
-	if (Object(ast.nodes).length) {
-		ast.nodes.forEach(node => {
-			if (isVarFunction(node)) {
-				const [propertyNode, comma, ...fallbacks] = node.nodes;
-				const propertyName = propertyNode.value;
+	const isValid = typeof ast?.walk === 'function';
 
-				if (propertyName in customProperties) {
-					return;
-				}
-
-				// conditionally test fallbacks
-				if (fallbacks.length) {
-					validateValueAST({ nodes: fallbacks.filter(isVarFunction) }, { result, customProperties, decl });
-
-					return;
-				}
-
-				// report unknown custom properties
-				stylelint.utils.report({
-					message: messages.unexpected(propertyName, decl.prop),
-					node: decl,
-					result,
-					ruleName,
-					word: String(propertyName),
-				});
-			} else {
-				validateValueAST(node, { result, customProperties, decl });
-			}
-		});
+	if (!isValid) {
+		return;
 	}
+
+	ast.walk(node => {
+		if (isVarFunction(node)) {
+			const [propertyNode, , ...fallbacks] = node.nodes;
+			const propertyName = propertyNode.value;
+
+			if (propertyName in customProperties) {
+				return;
+			}
+
+			// conditionally test fallbacks
+			if (fallbacks.length) {
+				validateValueAST({ nodes: fallbacks.filter(isVarFunction) }, { result, customProperties, decl });
+
+				return;
+			}
+
+			// report unknown custom properties
+			stylelint.utils.report({
+				message: messages.unexpected(propertyName, decl.prop),
+				node: decl,
+				result,
+				ruleName,
+				word: String(propertyName),
+			});
+		}
+	});
 };
 
 // whether the node is a var() function
-const isVarFunction = node => node.type === 'func' && node.name === 'var' && node.nodes[0].isVariable;
+const isVarFunction = node => node.type === 'function' && node.value === 'var' && node.nodes[0].value.startsWith('--');
